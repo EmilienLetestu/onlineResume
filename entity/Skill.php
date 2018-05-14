3b4404e9dc9c626b596a963c98e5d0daa78bf031
EmilienLetestu <eletestu@gmail.com>
2018-05-14 18:23:47 +0200

    [entity] skill entity

---
+++ entity/Skill.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 14/05/2018
+ * Time: 11:03
+ */
+
+class Skill
+{
+    /**
+     * @var
+     */
+    private $id;
+
+    /**
+     * @var
+     */
+    private $name;
+
+    /**
+     * @var
+     */
+    private $level;
+
+
+    /**
+     * @return int|null
+     */
+    public function getId() :?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getName() :?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int $level
+     */
+    public function setLevel(int $level)
+    {
+        $this->level = $level;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLevel() :?int
+    {
+        return $this->level;
+    }
+
+}
