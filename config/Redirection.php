eff61691a248d19b60fa994bad528a10b0547c29
EmilienLetestu <eletestu@gmail.com>
2018-05-15 14:17:11 +0200

    create a static function to handle redirection

---
+++ config/Redirection.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 15/05/2018
+ * Time: 12:18
+ */
+class Redirection
+{
+    /**
+     * @param string $path
+     * @return Redirection
+     */
+    public static function redirect(string $path) :self
+    {
+        header('Location:'.HOST.$path);
+    }
+}
