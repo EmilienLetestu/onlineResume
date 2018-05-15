05a19cb47f7271d14b0e8d68988c5a51781d8d87
EmilienLetestu <eletestu@gmail.com>
2018-05-15 14:55:46 +0200

    refactoring

--- formHandler/AddSkillHandler.php
+++ formHandler/AddSkillHandler.php
@@ -20,30 +20,6 @@ class AddSkillHandler
         return $level !== null && $name !== null;
     }
 
-    /**
-     * @return bool
-     */
-    private function isValid() :bool
-    {
-        if($this->isSubmitted()){
-
-            $name  = $_POST['skillName'];
-            $level = $_POST['skillLevel'];
-
-            $validator = new FormValidator();
-
-            $validationGroup = [
-                $validator->checkType($name,'string'),
-                $validator->checkType(intval($level), 'integer'),
-                $validator->checkLength($name,2,30),
-                $validator->checkRange(intval($level), 50, 100)
-            ];
-
-            return $validator->validate($validationGroup);
-        }
-
-        return false;
-    }
 
     /**
      * @param Skill $skill
@@ -51,7 +27,9 @@ class AddSkillHandler
      */
     public function handle(Skill $skill):bool
     {
-        if($this->isSubmitted() && $this->isValid()){
+        $validator = new AddSkillValidator();
+
+        if($this->isSubmitted() && $validator->isValid()){
 
             $skill->setName($_POST['skillName']);
             $skill->setLevel($_POST['skillLevel']);
---
+++ validator/AddSkillValidator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilien
+ * Date: 15/05/2018
+ * Time: 14:29
+ */
+
+class AddSkillValidator extends FormValidator
+{
+    /**
+     * @return bool
+     */
+    public function isValid() :bool
+    {
+           $name  = $_POST['skillName'];
+           $level = $_POST['skillLevel'];
+
+            $validationGroup = [
+                $this->checkType($name,'string'),
+                $this->checkType(intval($level), 'integer'),
+                $this->checkLength($name,2,30),
+                $this->checkRange(intval($level), 50, 100)
+            ];
+
+            return $this->validate($validationGroup);
+    }
+}
